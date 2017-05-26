85f99d1c177a7fadee862692cb405f554a80d532
atantet <alexis.tantet@gmail.com>
2017-05-26 14:09:02 +0200

    prout

--- search/searchdata.js
+++ search/searchdata.js
@@ -1,30 +1,12 @@
 var indexSectionsWithContent =
 {
-  0: "abcdefghijlmnopqrstuvw~",
-  1: "acefghjlmnprstv",
-  2: "cegorst",
-  3: "abcefghijlmnoprstuvw~",
-  4: "abcdefgijlmnopqrstvw",
-  5: "gst"
 };
 
 var indexSectionNames =
 {
-  0: "all",
-  1: "classes",
-  2: "files",
-  3: "functions",
-  4: "variables",
-  5: "groups"
 };
 
 var indexSectionLabels =
 {
-  0: "All",
-  1: "Classes",
-  2: "Files",
-  3: "Functions",
-  4: "Variables",
-  5: "Modules"
 };
 
